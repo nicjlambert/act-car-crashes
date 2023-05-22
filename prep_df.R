2893b6e2c13281e6252935b392fdebe368ce666c
lambert <lambni85@outlook.com>
2023-05-22 11:48:17 +0000

    updated script

--- prep_df.R
+++ prep_df.R
@@ -1,106 +1,59 @@
-# From CRAN:
-packages <- c(
-  "dplyr", "tidymodels", "RSocrata"
-  )
-#install.packages(packages)
+packages <- c("dplyr", "tidymodels", "RSocrata", "rpart", "rpart.plot")
 
-invisible(lapply(packages, library, character.only = TRUE))
-
-# source data set
-if (!file.exists("../large-files/act_bike_crash.rds")) {
-
- print("File not found. Downloading from 'https://www.data.act.gov.au' ...")
-
-  df <-
-  # Data
-  # at https://www.data.act.gov.au/Transport/ACT-Road-Crash-Data/6jn4-m8rx
-
-  RSocrata::read.socrata(
-    "https://www.data.act.gov.au/resource/bhhq-5z39.json"
-    , app_token = Sys.getenv("SOCRATA_API_TOKEN")
-    , email     = Sys.getenv("SOCRATA_API_EMAIL")
-    , password  = Sys.getenv("SOCRATA_API_PWD")
-   )
-
-  saveRDS(df, "../large-files/act_bike_crash.rds")
-  print("Done!")
-
-} else {
-
-print("File found. Loading...")
-
-df <-
- readRDS("../large-files/act_bike_crash.rds")
-print("Done!")
+# Check if packages are installed, and install them if necessary
+install_missing_packages <- function(packages) {
+  missing_packages <- packages[!sapply(packages, requireNamespace, quietly = TRUE)]
+  
+  if (length(missing_packages) > 0) {
+    install.packages(missing_packages)
+  }
 }
+
+# Install missing packages
+install_missing_packages(packages)
+
+# Load packages
+lapply(packages, library, character.only = TRUE)
+
+df <- RSocrata::read.socrata(
+    "https://www.data.act.gov.au/resource/6jn4-m8rx.json",
+    app_token = Sys.getenv("SOCRATA_TOKEN"),
+    email     = Sys.getenv("SOCRATA_EMAIL"),
+    password  = Sys.getenv("SOCRATA_PWD")
+  )
 
 #TODO model to predict crash severity?
 
 # inspect
 str(df)
 
-# read data and clean
-df_clean <- df %>%
-  as_tibble() %>%
-  # target outcome
-  mutate(is_fatal_or_injury = as.factor(
-    ifelse(cyclist_casualties >= 1, 1, 0)),
-         crash_date = as.Date(crash_date),
-         crash_hour = as.numeric(substr(crash_time, 1, 2))) %>%
-  mutate_if(is.character, as.factor) %>%
-  select(
-  -cyclist_casualties,
- -reported_location,
-  -crash_id,
-  -crash_date,
-  -crash_time,
-  -latitude,
-  -location_1.longitude,
-  -longitude,
-  -location_1.latitude,
-  -location_1.needs_recoding,
-  -location_1.human_address) %>%
-  na.omit()
+# Read the dataset
 
-str(df_clean)
+# Prepare the data
+# Remove unnecessary columns
+data <- df[, c("crash_type", "lighting_condition", "road_condition", "weather_condition", "crash_severity")]
 
-df_clean  %>%
-  count(is_fatal_or_injury) %>%
-  mutate(prop = n / sum(n))
+# Convert categorical variables to factors
+data$crash_type <- as.factor(data$crash_type)
+data$lighting_condition <- as.factor(data$lighting_condition)
+data$road_condition <- as.factor(data$road_condition)
+data$weather_condition <- as.factor(data$weather_condition)
 
-# split the data into trainng (75%) and testing (25%)
-df_split <- initial_split(df_clean, prop = 3 / 4, strata = is_fatal_or_injury)
+# Split the data into training and testing sets
+set.seed(123)  # For reproducibility
+train_index <- sample(nrow(data), 0.7 * nrow(data))  # 70% for training, adjust as desired
+train_data <- data[train_index, ]
+test_data <- data[-train_index, ]
 
-# extract training and testing sets
-df_train <- training(df_split)
-df_test <- testing(df_split)
+# Train the model
+model <- rpart(crash_severity ~ ., data = train_data, method = "class")
 
-lm_model <- logistic_reg() %>%
-    set_engine("glm") %>%
-    set_mode("classification")
+# Visualize the decision tree with a different color palette
+rpart.plot(model, box.palette = "Blues")
 
-lm_fit <- lm_model %>%
-    fit(is_fatal_or_injury ~ ., data = df_train)
+# Make predictions on the test set
+predictions <- predict(model, newdata = test_data, type = "class")
 
-class_preds <- lm_fit %>%
-    predict(new_data = df_test,
-            type = "class")
-
-# predict classification
-class_preds %>%
-  count(.pred_class) %>%
-  mutate(prop = n / sum(n))
-
-# predict probabilities
-prob_preds <- lm_fit %>%
-    predict(new_data = df_test,
-            type = "prob")
-
-leads_results <- df_test %>%
-    select(is_fatal_or_injury) %>%
-    bind_cols(class_preds, prob_preds)
-
-# We can now arrange these values into the 2-class confusion matrix:
-yardstick::conf_mat(leads_results,
-truth = is_fatal_or_injury,
-estimate = .pred_class)
+# Evaluate the model
+accuracy <- sum(predictions == test_data$crash_severity) / nrow(test_data)
+print(paste("Accuracy:", accuracy))
