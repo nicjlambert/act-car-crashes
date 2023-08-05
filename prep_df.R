26581b28588ffe817062885469d57a178cf31b32
lambert <lambni85@outlook.com>
2023-08-05 09:18:28 +0000

    included Dockerfile, updated prep_df.R

--- prep_df.R
+++ prep_df.R
@@ -1,4 +1,4 @@
-packages <- c("dplyr", "tidymodels", "RSocrata", "rpart", "rpart.plot")
+packages <- c("dplyr", "leaflet", "RSocrata")
 
 # Check if packages are installed, and install them if necessary
 install_missing_packages <- function(packages) {
@@ -22,57 +22,10 @@ data <- RSocrata::read.socrata(
     password  = Sys.getenv("SOCRATA_PWD")
   )
 
-# Convert factors to categorical variables
-#data$suburb_location <- as.factor(data$suburb_location)
-data$intersection <- as.factor(data$intersection)
-#data$midblock <- as.factor(data$midblock)
-#data$crash_direction <- as.factor(data$crash_direction)
-data$crash_type <- as.factor(data$crash_type)
-data$lighting_condition <- as.factor(data$lighting_condition)
-data$road_condition <- as.factor(data$road_condition)
-data$weather_condition <- as.factor(data$weather_condition)
-# Convert crash_time to a time object
-data$crash_time <- strptime(data$crash_time, format = "%H:%M")
+# Create the map
+m <- leaflet(data) %>%
+  addTiles() %>%
+  addCircleMarkers(~location.latitude, ~location.longitude, popup = ~as.character(crash_id))
 
-# Extract hour and minute
-data$hour <- as.integer(format(data$crash_time, "%H"))
-data$minute <- as.integer(format(data$crash_time, "%M"))
-
-# Check for missing data and handle it
-if (any(is.na(data))) {
-  data <- na.omit(data)
-}
-
-# Split the data into training and test sets
-set.seed(123)
-train_indices <- sample(1:nrow(data), nrow(data) * 0.7)
-train_data <- data[train_indices,]
-test_data <- data[-train_indices,]
-
-# Create a control function for cross-validation
-control <- rpart.control(cp = 0.01)  # Set the initial cp value
-
-# Perform 10-fold cross-validation to find the optimal cp value
-set.seed(123)
-fit <- rpart(crash_severity ~  intersection + crash_direction + crash_type + lighting_condition + road_condition + weather_condition + hour + minute, data = train_data, method = "class", weights = instance_weights, control = control)
-cv <- printcp(fit)  # Print the cross-validation results
-
-# Find the cp value that minimizes the cross-validation error
-best_cp <- fit$cptable[which.min(fit$cptable[, "xerror"]), "CP"]
-
-# Fit the final model using the best cp value
-crash_tree <- rpart(crash_severity ~  intersection + crash_direction + crash_type + lighting_condition + road_condition + weather_condition + hour + minute, data = train_data, method = "class", weights = instance_weights, control = rpart.control(cp = best_cp))
-
-# Plot the tree
-rpart.plot(crash_tree, extra = 1)
-
-# Make predictions on the test set
-predictions <- predict(crash_tree, newdata = test_data, type = "class")
-
-# Evaluate the model
-confusion_matrix <- table(observed = test_data$crash_severity, predicted = predictions)
-print(confusion_matrix)
-
-# Calculate accuracy
-accuracy <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
-print(paste("Accuracy: ", round(accuracy, 2)))
+# Display the map
+m
