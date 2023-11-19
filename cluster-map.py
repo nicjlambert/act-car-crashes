e8fff0258e20466e0662bb18563fa674610be291
lambert <lambni85@outlook.com>
2023-11-19 12:32:14 +0000

    removed timestepgeojson

--- cluster-map.py
+++ cluster-map.py
@@ -1,9 +1,8 @@
-import requests
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import folium
-from folium.plugins import TimestampedGeoJson
+import requests
 
 # Fetching and loading data
 url = 'https://www.data.act.gov.au/resource/6jn4-m8rx.json'
@@ -24,8 +23,6 @@ if not {'x', 'y', 'crash_severity', 'crash_date'}.issubset(df.columns):
 # Processing data
 df['latitude'] = pd.to_numeric(df['y'], errors='coerce')
 df['longitude'] = pd.to_numeric(df['x'], errors='coerce')
-df['crash_date'] = pd.to_datetime(df['crash_date'], errors='coerce')
-df['year'] = df['crash_date'].dt.year
 
 encoder = LabelEncoder()
 df['crash_severity_encoded'] = encoder.fit_transform(df['crash_severity'])
@@ -41,39 +38,19 @@ kmeans = KMeans(n_clusters=3, random_state=0)
 kmeans.fit(scaled_features)
 df['cluster'] = kmeans.labels_
 
-# Preparing data for TimestampedGeoJson
-features = []
-for year, group in df.groupby('year'):
-    for _, row in group.iterrows():
-        feature = {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [row['longitude'], row['latitude']],
-            },
-            'properties': {
-                'time': f"{year}-01-01",
-                'style': {'color': ''},
-                'icon': 'circle',
-                'iconstyle': {
-                    'fillColor': ['red', 'green', 'blue'][row['cluster']],
-                    'fillOpacity': 0.8,
-                    'stroke': 'true',
-                    'radius': 5
-                }
-            }
-        }
-        features.append(feature)
-
 # Creating a map
 map_center = [df['latitude'].mean(), df['longitude'].mean()]
 main_map = folium.Map(location=map_center, zoom_start=12)
 
-# Adding the Timestamped GeoJSON plugin
-TimestampedGeoJson({
-    'type': 'FeatureCollection',
-    'features': features,
-}, period='P1Y', add_last_point=True).add_to(main_map)
+# Adding cluster markers to the map
+for _, row in df.iterrows():
+    folium.CircleMarker(
+        location=[row['latitude'], row['longitude']],
+        radius=5,
+        color=['red', 'green', 'blue'][row['cluster']],
+        fill=True,
+        fill_opacity=0.8
+    ).add_to(main_map)
 
-main_map.save('map_with_yearly_slider.html')
+main_map.save('map_with_clusters.html')
 print("Map with traffic crash clusters created successfully.")
