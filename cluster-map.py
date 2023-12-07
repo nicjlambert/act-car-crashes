2199bc7c01684f06c535e290a94cb726e5c84b92
lambert <lambni85@outlook.com>
2023-12-07 12:07:51 +0000

    misc. updates

--- cluster-map.py
+++ cluster-map.py
@@ -1,74 +1,64 @@
 import os
-import json
-import numpy as np
-import pandas as pd
-import json
-from mapboxgl.viz import ClusteredCircleViz
-from mapboxgl.utils import *
 import requests
+import pandas as pd
+import geojson
+from mapboxgl.viz import CircleViz
+from mapboxgl.utils import create_color_stops
 
-# Further info:
-#   https://github.com/mapbox/mapboxgl-jupyter
+def fetch_data(url, local_cache_file):
+    if os.path.exists(local_cache_file):
+        with open(local_cache_file, 'r') as file:
+            data = json.load(file)
+    else:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            with open(local_cache_file, 'w') as file:
+                json.dump(data, file)
+        else:
+            raise ConnectionError(f"Failed to retrieve data. Status code: {response.status_code}")
+    return data
 
-# Fetching and loading data
+def preprocess_data(data):
+    df = pd.DataFrame(data)
+    crash_counts = df.groupby('suburb_location').size().reset_index(name='crash_count')
+    df = df.merge(crash_counts, on='suburb_location', how='left')
+    high_risk_threshold = df['crash_count'].quantile(0.75)
+    df['risk_level'] = np.where(df['crash_count'] >= high_risk_threshold, 'High', 'Normal')
+    return df
+
+def create_geojson(df):
+    features = [
+        geojson.Feature(
+            geometry=geojson.Point((float(item["location"]["longitude"]), float(item["location"]["latitude"]))),
+            properties={key: item[key] for key in item if key != 'location'}
+        )
+        for item in df.to_dict(orient='records')
+    ]
+    return geojson.FeatureCollection(features)
+
+def create_viz(geojson_data, access_token):
+    count_color_stops = create_color_stops([1, 10, 50, 100], colors='Blues')
+    count_viz = CircleViz(
+        geojson_data,
+        access_token=access_token,
+        color_property="crash_count",
+        color_stops=count_color_stops,
+        center=(149, -35.30),
+        zoom=10
+    )
+    return count_viz
+
+# Main workflow
 url = 'https://www.data.act.gov.au/resource/6jn4-m8rx.json'
-response = requests.get(url)
-
-if response.status_code != 200:
-    print("Failed to retrieve data. Status code:", response.status_code)
-    exit()
-
-data = response.json()
-df = pd.DataFrame(data)
-
-# Group by 'suburb_location' and create a new DataFrame with crash counts
-crash_counts = df.groupby('suburb_location').size().reset_index(name='crash_count')
-
-# Merge this back into the original DataFrame
-df = df.merge(crash_counts, on='suburb_location', how='left')
-
-# Now you have the 'crash_count' column in df and can create the 'risk_level' column
-high_risk_threshold = df['crash_count'].quantile(0.75) # Or any other statistical measure
-df['risk_level'] = np.where(df['crash_count'] >= high_risk_threshold, 'High', 'Normal')
-
-# Convert the DataFrame to a JSON string
-json_data = df.to_json(orient='records')
-
-# Parse the JSON string back into a list of dictionaries, which resembles the parsed JSON data
-data = json.loads(json_data)
-
-# Access Mapbox Access Token from environment variable
+local_cache_file = 'data_cache.json'
 mapbox_access_token = os.getenv('MAPBOX_ACCESS_TOKEN')
-
-# Ensure the token is available
 if mapbox_access_token is None:
     raise ValueError("Mapbox access token not found in environment variables.")
 
-# Convert data to GeoJSON
-features = []
-for item in data:
-    point = geojson.Point((float(item["location"]["longitude"]), float(item["location"]["latitude"])))
-    properties = {key: item[key] for key in item if key != 'location'}
-    features.append(geojson.Feature(geometry=point, properties=properties))
-
-geojson_data = geojson.FeatureCollection(features)
-
-# Assign color stops
-color_stops = create_color_stops([1,10,50,100], colors='BrBG')
-
-# Create the visualization
-viz = ClusteredCircleViz(geojson_data,
-                access_token=mapbox_access_token,
-                style='mapbox://styles/mapbox/light-v10',
-                center=(149, -35.30),
-                color_stops = color_stops,
-                radius_stops = [[1,5], [10, 10], [50, 15], [100, 20]],
-                cluster_maxzoom = 10,
-                cluster_radius = 25,
-                opacity = 0.9,
-                zoom = 10
-                )
-
-viz.scale = True
-# Show the map
+# Fetch, preprocess data, and create visualization
+data = fetch_data(url, local_cache_file)
+df = preprocess_data(data)
+geojson_data = create_geojson(df)
+viz = create_viz(geojson_data, mapbox_access_token)
 viz.show()
